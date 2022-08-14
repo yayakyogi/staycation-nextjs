08507d31764a4a992a7f2000eb488e1d80526dd0
yayakyogi <yayaktaka@gmail.com>
2022-08-15 05:37:56 +0700

    add style

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
     "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "poppins-light": ["Poppins-Light,sans-serif"],
+        "poppins-regular": ["Poppins-Regular,sans-serif"],
+        "poppins-medium": ["Poppins-Medium,sans-serif"],
+        "poppins-bold": ["Poppins-Bold,sans-serif"],
+      },
+      textColor: {
+        primary: "#152C5B",
+        secondary: "#B0B0B0",
+        active: "#3252DF",
+      },
+      backgroundColor: {
+        primary: "#3252DF",
+        secondary: "#F5F6F8",
+        red: "#E74C3C",
+        green: "#1ABC9C",
+      },
+    },
   },
   plugins: [],
 };
