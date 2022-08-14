455cab062f336a86614815c1db4b03be53a4779f
yayakyogi <yayaktaka@gmail.com>
2022-08-15 05:38:18 +0700

    removefiile

--- components/Footer/index.tsx
+++
@@ -1,9 +0,0 @@
-import style from "./footer.module.css";
-
-export default function Footer() {
-  return (
-    <div className={style.footer}>
-      Copyright &copy; 2022 - Yayak Yogi Ginantaka
-    </div>
-  );
-}
