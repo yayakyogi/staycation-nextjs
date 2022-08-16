42259712cbbb4affd9e135f181935b0fa3447713
yayakyogi <yayaktaka@gmail.com>
2022-08-16 12:11:58 +0700

    add import component

--- components/Organism/index.tsx
+++ components/Organism/index.tsx
@@ -1,4 +1,7 @@
 import Hero from "./Hero";
 import MostPicked from "./MostPicked";
+import Categories from "./Categories";
+import Testimonial from "./Testimonial";
+import Footer from "./Footer";
 
-export { Hero, MostPicked };
+export { Hero, MostPicked, Categories, Testimonial, Footer };
