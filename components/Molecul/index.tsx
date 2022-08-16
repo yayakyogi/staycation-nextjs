e5fe4e4ef8e041285738ab92d63c14f7a429e2dd
yayakyogi <yayaktaka@gmail.com>
2022-08-16 07:10:04 +0700

    add export molecul component

---
+++ components/Molecul/index.tsx
@@ -0,0 +1,6 @@
+import HeroItem from "./HeroItem";
+import Header from "./Navbar";
+import Footer from "./Footer";
+import CardMostPicked from "./CardMostpicked";
+
+export { Header, Footer, HeroItem, CardMostPicked };
