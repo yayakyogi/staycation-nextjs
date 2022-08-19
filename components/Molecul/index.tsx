d291f564db003b5a09d7bf65fd7a54518071293e
yayakyogi <yayaktaka@gmail.com>
2022-08-19 21:51:07 +0700

    refactor component booking card

--- components/Molecul/index.tsx
+++ components/Molecul/index.tsx
@@ -1,6 +1,17 @@
-import HeroItem from "./HeroItem";
-import Header from "./Navbar";
+import ItemIcon from "./ItemIcon";
 import CardMostPicked from "./CardMostpicked";
 import CardCategories from "./CardCategories";
+import BookingPrice from "./BookingPrice";
+import BookingLong from "./BookingLong";
+import BookingDate from "./BookingDate";
+import BookingPayment from "./BookingPayment";
 
-export { Header, HeroItem, CardMostPicked, CardCategories };
+export {
+  ItemIcon,
+  CardMostPicked,
+  CardCategories,
+  BookingPrice,
+  BookingLong,
+  BookingDate,
+  BookingPayment,
+};
