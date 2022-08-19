c100ac2e7e9ce4ad8353352462e2163c014612e7
yayakyogi <yayaktaka@gmail.com>
2022-08-19 21:49:29 +0700

    add fonts and color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,18 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
+        "poppins-extralight": ["Poppins-ExtraLight,sans-serif"],
         "poppins-light": ["Poppins-Light,sans-serif"],
         "poppins-regular": ["Poppins-Regular,sans-serif"],
         "poppins-medium": ["Poppins-Medium,sans-serif"],
+        "poppins-semibold": ["Poppins-SemiBold,sans-serif"],
         "poppins-bold": ["Poppins-Bold,sans-serif"],
       },
       textColor: {
         primary: "#152C5B",
         secondary: "#B0B0B0",
         active: "#3252DF",
+        green: "#1ABC9C",
       },
       backgroundColor: {
         primary: "#3252DF",
@@ -23,6 +26,7 @@ module.exports = {
         red: "#E74C3C",
         green: "#1ABC9C",
         pink: "#FF498B",
+        dark: "#152C5B",
       },
     },
   },
