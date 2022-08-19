153adf3422dc3c2986ca10f2420c61e0e6d0ed02
yayakyogi <yayaktaka@gmail.com>
2022-08-19 21:54:39 +0700

    export component booking card

--- components/Organism/index.tsx
+++ components/Organism/index.tsx
@@ -1,7 +1,23 @@
+import Navbar from "./Navbar";
 import Hero from "./Hero";
 import MostPicked from "./MostPicked";
 import Categories from "./Categories";
 import Testimonial from "./Testimonial";
 import Footer from "./Footer";
+import Breadcrumb from "./Breadcrumb";
+import DetailPhotos from "./DetailPhotos";
+import DetailAbout from "./DetailAbout";
+import BookingCard from "./BookingCard";
 
-export { Hero, MostPicked, Categories, Testimonial, Footer };
+export {
+  Navbar,
+  Hero,
+  MostPicked,
+  Categories,
+  Testimonial,
+  Footer,
+  Breadcrumb,
+  DetailPhotos,
+  DetailAbout,
+  BookingCard,
+};
