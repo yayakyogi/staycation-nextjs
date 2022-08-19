3f4fd66fd64b4a85a602ce4c89c6af2ead705c57
yayakyogi <yayaktaka@gmail.com>
2022-08-19 21:55:23 +0700

    remove padding

--- components/Organism/Categories/index.tsx
+++ components/Organism/Categories/index.tsx
@@ -2,12 +2,13 @@ import { CardCategories } from "../../Molecul";
 
 export default function Categories() {
   return (
-    <div className="px-44 mb-20">
+    <div className="mb-20">
       <h1 className="font-poppins-medium text-2xl text-primary mb-5">
         Houses with beauty backyard
       </h1>
       <div className="flex justify-between items-center">
         <CardCategories
+          route="/detail/1"
           image="/images/img-category-1.png"
           placeName="Tabby Town"
           city="Gunung Batu"
@@ -15,18 +16,21 @@ export default function Categories() {
           isPopular
         />
         <CardCategories
+          route="/detail/2"
           image="/images/img-category-2.png"
           placeName="Anggana"
           city="Bogor"
           country="Indonesia"
         />
         <CardCategories
+          route="/detail/3"
           image="/images/img-category-3.png"
           placeName="Seattle Rain"
           city="Jakarta"
           country="Indonesia"
         />
         <CardCategories
+          route="/detail/4"
           image="/images/img-category-4.png"
           placeName="Wodden Pit"
           city="Wonosobo"
