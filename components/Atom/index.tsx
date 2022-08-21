5b1bc91eeed737f2d97451608f0871c9581d17a2
yayakyogi <yayaktaka@gmail.com>
2022-08-22 00:27:33 +0700

    add indexController and texfield

--- components/Atom/index.tsx
+++ components/Atom/index.tsx
@@ -1,5 +1,7 @@
 import Title from "./Title";
 import ButtonLink from "./ButtonLink";
 import ButtonSubmit from "./ButtonSubmit";
+import IndexController from "./IndexController";
+import TextField from "./Textfield";
 
-export { Title, ButtonLink, ButtonSubmit };
+export { Title, ButtonLink, ButtonSubmit, IndexController, TextField };
